7501a825d4c0311ae3dbeb721464e8f84b5c8b01
Benedicta Oghenevoke Egbikuadje <BE204@live.mdx.ac.uk>
2022-10-07 16:40:36 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import products from '../products.json'
+
+
+export default{
+  data(){
+    return{
+      data: products,
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
